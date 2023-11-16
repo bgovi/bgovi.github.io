7dfd098ac4b6d956aca9131028e383e096ea7082
Brandon Govindarajoo <bgovi@umich.edu>
2023-11-16 05:28:10 -0500

    Added image placeholder to mdm

--- bgovi/pages/projects/mdm.vue
+++ bgovi/pages/projects/mdm.vue
@@ -1,11 +1,39 @@
 <template>
-<div>
-<h1>over view image</h1>
-<h1>data hooks</h1>
-<h1>config as code</h1>
-<h1>conditional changes</h1>
-<h1>as of query</h1>
-<h1>extensions, versions and rewrites</h1>
+<!-- <figure class="image is-16by9">
+  <iframe class="has-ratio" width="640" height="360" src="https://www.youtube.com/embed/YE7VzlLtp-4" frameborder="0" allowfullscreen></iframe>
+</figure> -->
 
+
+
+<div class="container is-max-desktop">
+  <div class="image is-16x9">
+    <img src="https://bulma.io/images/placeholders/256x256.png" alt="Mobile Image">
+  </div>
 </div>
+
+
+  <!-- <div class="image-container">
+    <figure class="image is-16by9">
+      <img 
+        srcset="image-mobile.jpg 480w,
+                image-tablet.jpg 768w,
+                image-desktop.jpg 1024w,
+                image-widescreen.jpg 1216w,
+                image-fullhd.jpg 1408w"
+        sizes="(max-width: 480px) 100vw,
+               (max-width: 768px) 100vw,
+               (max-width: 1024px) 100vw,
+               (max-width: 1216px) 100vw,
+               1408px"
+        src="image-fullhd.jpg"
+        alt="Your Image"
+      >
+    </figure>
+  </div> -->
+
+
+
+<!-- is-half
+is-offset-one-quarter -->
+
 </template>
